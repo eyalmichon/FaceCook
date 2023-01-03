600f16053c06b514b9404b81d4684638af0838f8
Maoz <maoz123@live.com>
2023-01-03 15:23:13 +0200

    search js

--- public/js/searchRecipes.js
+++ public/js/searchRecipes.js
@@ -1,4 +1,92 @@
 $(document).ready(function () {
+    
+
+    function showNutritionLabel(data) {
+        $('.nutrition-label').remove();
+
+        // create a new element for the nutrition label
+        var nutritionLabel = $('<div class="nutrition-label">\
+        <h3>Nutrition Facts</h3>\
+        <hr>\
+        <p>Category: <span class="category"></span></p>\
+        <p>Serving Size: <span class="serving-size"></span></p>\
+        <hr>\
+        <table>\
+            <tr>\
+            <td>Calories :</td>\
+            <td class="calories"></td>\
+            </tr>\
+            <tr>\
+            <td>Total Fat :</td>\
+            <td class="total_fat"></td>\
+            </tr>\
+            <tr>\
+            <td>Sodium :</td>\
+            <td class="sodium"></td>\
+            </tr>\
+            <tr>\
+            <td>Carbohydrates :</td>\
+            <td class="carbohydrates"></td>\
+            </tr>\
+            <tr>\
+            <td>Protein :</td>\
+            <td class="protein"></td>\
+            </tr>\
+            <tr>\
+            <td>Sugars :</td>\
+            <td class="sugars"></td>\
+            </tr>\
+            <tr>\
+            <td>Saturated fat :</td>\
+            <td class="saturated_fat"></td>\
+            </tr>\
+        </table>\
+        </div>');
+    
+        $("body").append(nutritionLabel);
+
+        $('.category').text(data.category);
+        $('.sodium').text(data.sodium);
+        $('.total_fat').text(data.total_fat);
+        $('.carbohydrates').text(data.carbohydrates);
+        $('.protein').text(data.protein);
+        $('.calories').text(data.kcal);
+        $('.sugars').text(data.sugars);
+        $('.saturated_fat').text(data.saturated_fat);
+    }
+
+    $("form").submit(async function (event) {
+        // prevent the form from submitting the default way
+        event.preventDefault();
+
+        // showNutritionLabel();
+
+        // console.log(filter.search)  
+
+        // get the recipe with reviews
+        fetch('/getRecipeWithReviews', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              search: $('#searchTerm').val(),
+            })
+          })
+            .then(response => response.json())
+            .then(data => {
+              if (data.error) {
+                throw new Error(data.error);
+              }
+                showNutritionLabel(data[0]);
+            })
+            .catch(error => {
+              console.log(error);
+              return;
+            });
+
+    });
+
     $("#searchTerm").autocomplete({
         source: function (request, response) {
             $.ajax({
